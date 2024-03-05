352ff985b49d744a78956e45d603ef731f7621ba
this-iskamal <kamal.gautam.36808@gmail.com>
2024-03-05 16:58:16 +0545

    created signup route

--- api/index.js
+++ api/index.js
@@ -1,21 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
-const app = express();
-
 mongoose
-  .connect(
-    process.env.MONGO_URL
-  )
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("Connected to MongoDB");
-  }).catch((err) => {
+  })
+  .catch((err) => {
     console.log("Error: ", err);
   });
+
+const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes)
